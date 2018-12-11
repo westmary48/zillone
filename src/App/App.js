b50c8f3b2c9123161591c2063ee5ce425b1a2eea
Mary West <westmarye1219@gmail.com>
2018-12-11 17:50:46 -0600

    setup

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Button } from 'reactstrap';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
+    console.log(this);
     return (
       <div className="App">
         <header className="App-header">
@@ -19,6 +21,16 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button className='btn btn-danger'>HELP ME</button>
+          <Button
+          tag="a"
+          color="success"
+          size="large"
+          href="http://reactstrap.github.io"
+          target="_blank"
+>
+        View Reactstrap Docs
+        </Button>
         </header>
       </div>
     );
