10dda5dd6e2753fab7c241ca74d30adae9a5c0a8
Mary West <westmarye1219@gmail.com>
2018-12-15 11:00:14 -0600

    fixed listings

--- src/components/Listings/Listings.js
+++ src/components/Listings/Listings.js
@@ -2,15 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Listings.scss';
 import listingShape from '../../helpers/propz/listingShape';
+import ListingItem from '../ListingItem/ListingItem';
 
 class Listings extends React.Component {
   static PropTypes = {
-    listings:PropTypes.arrayOf(listingShape);
+    listings: PropTypes.arrayOf(listingShape),
+  }
 
   render() {
+    const { listings } = this.props;
+    const listingsItemComponents = listings.map(listing => (
+    <ListingItem
+    key= {listing.id}
+    listing = {listing}
+    />
+    ));
     return (
             <div className = "listings">
             <h2>Listings</h2>
+            {listingsItemComponents}
             </div>
     );
   }
